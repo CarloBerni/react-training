4961d281cbf2fc1fb33ae25e20ed338f75124a7c
CarloBerni <carlo.berni@hetic.net>
2019-03-31 18:34:31 +0200

    beginning

---
+++ build/precache-manifest.e3d85dc848054ca6a7fa4c77ec1399d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f76050cc47769393abb3",
+    "url": "/static/js/main.bebe8c2c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f76050cc47769393abb3",
+    "url": "/static/css/main.cd21a7a6.chunk.css"
+  },
+  {
+    "revision": "c281e9a2054fd12ba90c6bb3c525d46f",
+    "url": "/index.html"
+  }
+];
